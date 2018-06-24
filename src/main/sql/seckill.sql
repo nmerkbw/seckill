67d5ee8674f2467d44a82aedc691b687abf581c7
Jiangxs <songxjxs@yeah.ent>
2018-06-24 22:48:39 +0800

    更新sql的存储过程，state状态值会改变

--- src/main/sql/seckill.sql
+++ src/main/sql/seckill.sql
@@ -58,3 +58,50 @@ SELECT  @r_result;
 -- 2.不要过度依赖存储过程
 -- 3.简单的逻辑可以应用存储过程
 -- 4.该QPS：一个秒杀单6000/qps
+
+
+-- 上述存储过程未对state状态进行赋值，导致秒杀成功后状态为-1
+DELIMITER $$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE
+  `execute_seckillnew`(IN  v_seckill_id BIGINT,
+                       IN v_phone BIGINT,
+                       IN v_kill_time TIMESTAMP,
+                       OUT r_result INT)
+  BEGIN
+    DECLARE insert_count INT DEFAULT 0;
+    START TRANSACTION;
+    INSERT IGNORE INTO success_killed
+    (seckill_id,user_phone,create_time,state)
+    VALUES (v_seckill_id,v_phone,v_kill_time,0);
+    SELECT row_count() INTO insert_count;
+    IF (insert_count = 0) THEN
+      ROLLBACK;
+      SET r_result = -1;
+    ELSEIF (insert_count < 0) THEN
+      ROLLBACK;
+      SET r_result = -2;
+    ELSE
+      UPDATE seckill
+      SET number = number - 1
+      WHERE seckill_id = v_seckill_id
+            AND end_time > v_kill_time
+            AND start_time < v_kill_time
+            AND number > 0;
+      SELECT row_count() INTO insert_count;
+      IF (insert_count = 0) THEN
+        ROLLBACK;
+        SET r_result = 0;
+      ELSEIF (insert_count < 0) THEN
+        ROLLBACK;
+        SET r_result = -2;
+      ELSE
+        COMMIT;
+        SET r_result = 1;
+      END IF;
+    END IF;
+  END
+$$
+-- 存储过程定义结束
+
+DELIMITER ;
